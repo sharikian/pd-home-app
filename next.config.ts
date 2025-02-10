4abdc2fb072ae1dda15adc48e5ae809c8c5139f6
sharikian <wacoinseig@gmail.com>
2025-02-10 12:30:57 +0330

    dynamic next config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+const repo_path = '/online-pd'
 const nextConfig: NextConfig = {
-  // basePath: "/online-pd",
-  // output: "export",
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: isProd ? repo_path : '',
+  basePath: isProd ? repo_path : '',
+  output: "export",
   reactStrictMode: true,
 };
 
